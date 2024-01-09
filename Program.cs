43175cab230645193de65487af644c1e087a59eb
Tony <antony.branquinho@gmail.com>
2024-01-09 17:00:47 -0300

    precisei de ajuda por causa do fatorial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite um numero.");
+            int n = int.Parse(Console.ReadLine());
+
+            int fatorial = 1;
+            for (int i = 1; i <= n; i++) {
+                fatorial = fatorial *  i;
+            }
+
+            Console.WriteLine(fatorial);
+        }
+    }
+}
